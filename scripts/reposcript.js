92558f8228f9f9df8df33390293618772c20fd5b
yousef239 <yabed96@gmail.com>
2018-10-12 09:38:01 -0400

    Change UI

--- scripts/reposcript.js
+++ scripts/reposcript.js
@@ -6,8 +6,9 @@ img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
+	}
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
-}
+	}
+	
