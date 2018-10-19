bccfb078aed3ddd1e62c8a3ea0d263455582f76e
yousef239 <yabed96@gmail.com>
2018-10-19 12:49:13 -0400

    update goal change link in projects

--- scripts/reposcript.js
+++ scripts/reposcript.js
@@ -2,13 +2,18 @@ var modal = document.getElementById('myModal');
 var img = document.getElementById('skydiveimg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption1");
+
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 	}
+
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 	}
 	
+function displayDate() {
+	document.getElementById("time").innerHTML = Date();
+	}
